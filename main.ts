58dfa5a84998d52b5fdafacb875f4cca032d1197
Mary242524 <am.mzapatero25b@corazonistas.com>
2024-11-19 13:13:38 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    Coordenada_X += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.setGroup(Coordenada_X * 10 + Coordenada_Y)
+    radio.sendString("MARÍA")
+})
 radio.onReceivedString(function (receivedString) {
     led.plot(parseFloat(receivedString.substr(0, 1)), parseFloat(receivedString.substr(1, 1)))
     basic.pause(10000)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    Coordenada_Y += 1
+})
+let Coordenada_Y = 0
+let Coordenada_X = 0
 radio.setGroup(1)
